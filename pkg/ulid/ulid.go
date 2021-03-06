022b07f6eb354cad5f37664c19d12803b2156ecb
Michael Graff <explorer@flame.org>
2021-03-05 20:40:39 -0600

    use Now(), and remove tunnel dependency

--- pkg/ulid/ulid.go
+++ pkg/ulid/ulid.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/oklog/ulid/v2"
-	"github.com/opsmx/grpc-bidir/pkg/tunnel"
 )
 
 // Context holds the state needed to generate a ULID using random values.  This
@@ -19,14 +18,15 @@ type Context struct {
 
 // NewContext returns the context needed for subsequent calls.
 func NewContext() *Context {
-	t := time.Unix(1000000, 0)
-	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	t := time.Now().UnixNano()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t)), 0)
 	return &Context{entropy: entropy}
 }
 
 // Ulid - return a new ULID as a string.
 func (ctx *Context) Ulid() string {
+	t := time.Now().UnixNano()
 	ctx.Lock()
 	defer ctx.Unlock()
-	return ulid.MustNew(tunnel.Now(), ctx.entropy).String()
+	return ulid.MustNew(uint64(t), ctx.entropy).String()
 }
