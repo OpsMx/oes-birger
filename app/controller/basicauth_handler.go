113639cc08e931f3601df25eebf4525f880ad366
Michael Graff <explorer@flame.org>
2021-04-06 06:20:34 -0500

    remove username check, it's redundant and pointless

--- app/controller/basicauth_handler.go
+++ app/controller/basicauth_handler.go
@@ -18,50 +18,25 @@ func getAuthParts(username string) (epType string, epName string, agent string, 
 }
 
 func basicAuthAPIHandler(serviceType string, w http.ResponseWriter, r *http.Request) {
-	var authUsername string
 	var authPassword string
 	var ok bool
-	if authUsername, authPassword, ok = r.BasicAuth(); !ok {
-		log.Printf("No credentials provided, epType %s", serviceType)
-	}
-
-	// Pull fields from the username, which is of the format
-	// eptype.epname.agentid
-	usernameType, usernameName, usernameAgent, err := getAuthParts(authUsername)
-	if err != nil {
-		log.Printf("%v", err)
-		w.WriteHeader(http.StatusForbidden)
-		return
+	if _, authPassword, ok = r.BasicAuth(); !ok {
+		log.Printf("No credentials provided, endpointType %s", serviceType)
 	}
 
 	// Pull fields from the password, and if they validate, compare to the
 	// username.
-	epType, epName, agentIdentity, err := ValidateJWT(jwtKeyset, authPassword)
+	endpointType, endpointName, agentIdentity, err := ValidateJWT(jwtKeyset, authPassword)
 	if err != nil {
 		log.Printf("%v", err)
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-	if usernameType != epType {
-		log.Printf("usernameType %s does not match JWT field %s", usernameType, epType)
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-	if usernameName != epName {
-		log.Printf("usernameName %s does not match JWT field %s", usernameName, epName)
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-	if usernameAgent != agentIdentity {
-		log.Printf("usernameAgent %s does not match JWT field %s", usernameAgent, agentIdentity)
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
 	ep := agent.AgentSearch{
 		Identity:     agentIdentity,
-		EndpointType: epType,
-		EndpointName: epName,
+		EndpointType: endpointType,
+		EndpointName: endpointName,
 	}
 	runAPIHandler(ep, w, r)
 }
