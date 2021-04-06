7b79489aa78cd0ec2a3da1010243079f19c8f0cc
Michael Graff <explorer@flame.org>
2021-04-05 20:09:51 -0500

    clean up a bit

--- app/controller/jwt.go
+++ app/controller/jwt.go
@@ -8,12 +8,18 @@ import (
 	"github.com/lestrrat-go/jwx/jwt"
 )
 
+const (
+	JWTEndpointTypeKey = "t"
+	JWTEndpointNameKey = "n"
+	JWTAgentKey        = "a"
+)
+
 func MakeJWT(key jwk.Key, epType string, epName string, agent string) (string, error) {
 	t := jwt.New()
 	t.Set(jwt.IssuerKey, "opsmx")
-	t.Set("t", epType)
-	t.Set("n", epName)
-	t.Set("a", agent)
+	t.Set(JWTEndpointTypeKey, epType)
+	t.Set(JWTEndpointNameKey, epName)
+	t.Set(JWTAgentKey, agent)
 
 	signed, err := jwt.Sign(t, jwa.HS256, key)
 	if err != nil {
@@ -31,17 +37,17 @@ func ValidateJWT(keyset jwk.Set, tokenString string) (epType string, epName stri
 	if err != nil {
 		return
 	}
-	if itype, ok := token.Get("t"); ok {
+	if itype, ok := token.Get(JWTEndpointTypeKey); ok {
 		epType = itype.(string)
 	} else {
 		return "", "", "", fmt.Errorf("missing epType")
 	}
-	if iname, ok := token.Get("n"); ok {
+	if iname, ok := token.Get(JWTEndpointNameKey); ok {
 		epName = iname.(string)
 	} else {
 		return "", "", "", fmt.Errorf("missing epName")
 	}
-	if iagent, ok := token.Get("a"); ok {
+	if iagent, ok := token.Get(JWTAgentKey); ok {
 		agent = iagent.(string)
 	} else {
 		return "", "", "", fmt.Errorf("missing agent")
