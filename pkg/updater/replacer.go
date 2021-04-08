a531c0e2ceb124244f6ad5e1a2858fd33a1d8b5e
Michael Graff <explorer@flame.org>
2021-04-08 08:55:28 -0500

    add a self-exec to replace our binary, once we update

--- pkg/updater/replacer.go
+++ pkg/updater/replacer.go
@@ -1,6 +1,9 @@
 package updater
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 func HashSelf() (string, error) {
 	selfPath := os.Args[0]
@@ -10,4 +13,11 @@ func HashSelf() (string, error) {
 		return "", err
 	}
 	return hash.String(), nil
+}
+
+func RestartSelf(path string) error {
+	if err := syscall.Exec(os.Args[0], os.Args, os.Environ()); err != nil {
+		return err
+	}
+	return nil
 }
