00c5d680aecf3a4178f9897fd41c53d92b8407fa
Michael Graff <explorer@flame.org>
2021-05-03 19:14:07 -0500

    bump version

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -50,7 +50,7 @@ const (
 
 var (
 	versionBuild = -1
-	version      = util.Versions{Major: 2, Minor: 1, Patch: 0, Build: versionBuild}
+	version      = util.Versions{Major: 2, Minor: 1, Patch: 1, Build: versionBuild}
 
 	configFile = flag.String("configFile", "/app/config/config.yaml", "The file with the controller config")
 
