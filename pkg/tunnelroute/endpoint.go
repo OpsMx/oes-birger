aa8c32a11847022c3c4f8fd794e560dc1e2e4429
Michael Graff <explorer@flame.org>
2022-02-03 13:34:23 -0600

    also show more endpoint details here

--- pkg/tunnelroute/endpoint.go
+++ pkg/tunnelroute/endpoint.go
@@ -33,5 +33,5 @@ type Endpoint struct {
 }
 
 func (e *Endpoint) String() string {
-	return fmt.Sprintf("(%s, %s, %v)", e.Type, e.Name, e.Configured)
+	return fmt.Sprintf("(type=%s, name=%s, configured=%v)", e.Type, e.Name, e.Configured)
 }
