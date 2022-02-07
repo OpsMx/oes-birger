f17406d40b66d030f2aad310f69db7c3b8382736
Michael Graff <explorer@flame.org>
2022-02-07 17:22:29 -0600

    fix api depricated warning

--- app/agent/main.go
+++ app/agent/main.go
@@ -33,6 +33,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/opsmx/oes-birger/internal/ca"
 	"github.com/opsmx/oes-birger/internal/secrets"
@@ -160,7 +161,7 @@ func main() {
 	}
 
 	if config.InsecureControllerAllowed {
-		opts = append(opts, grpc.WithInsecure())
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	} else {
 		opts = append(opts, grpc.WithTransportCredentials(ta))
 	}
